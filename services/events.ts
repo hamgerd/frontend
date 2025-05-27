3a8497d434839954130cc951cebec5f592626c5a
Jack <jack.70aw@gmail.com>
2025-05-27 20:11:19 +0330

    fix deploy

--- services/events.ts
+++ services/events.ts
@@ -1,10 +1,10 @@
-import { Event } from "@/models";
-import api from "@/lib/axios";
+// import { Event } from "@/models";
+// import api from "@/lib/axios";
 
-export const EventServices = {
-  async getAll(): Promise<Event[]> {
-// FIXME 
-    const response = await api.get<Event[]>("events/");
-    return response.data;
-  },
-};
+// export const EventServices = {
+//   async getAll(): Promise<Event[]> {
+// // FIXME
+//     const response = await api.get<Event[]>("events/");
+//     return response.data;
+//   },
+// };
