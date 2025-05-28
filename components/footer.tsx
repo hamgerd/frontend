b5775b8336307bab1b0bed1234c6a06619e52072
Jack <jack.70aw@gmail.com>
2025-05-28 11:56:49 +0330

    add enamad

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,122 +5,138 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="border-t py-10 bg-muted/30 w-full flex flex-col  ">
-      <div className="container grid grid-cols-1 md:grid-cols-4 gap-8 px-10 mx-auto">
-        <div className="space-y-4 flex justify-center flex-col">
-          <div className="flex items-center gap-2">
-            <Image
-              src={"/hamgerd-logo.svg"}
-              alt="logo"
-              width={60}
-              height={60}
-              className="ml-2 dark:invert"
-            />
+    <footer className="border-t py-10 bg-muted/30 w-full flex flex-col ">
+      <div className="flex">
+        <div className="container grid grid-cols-1 md:grid-cols-5 gap-10 px-6 mx-auto">
+          <div className="space-y-4 flex justify-center flex-col">
+            <div className="flex items-center gap-2">
+              <Image
+                src={"/hamgerd-logo.svg"}
+                alt="logo"
+                width={60}
+                height={60}
+                className="ml-2 dark:invert"
+              />
+            </div>
+            <p className="text-sm text-muted-foreground">
+              پلتفرم جامع برگزاری و مدیریت رویدادهای آموزشی، علمی و تجاری
+            </p>
+            <div className="flex justify-center gap-6">
+              <Link
+                href="https://t.me/hamgerd_ir"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                <FaTelegram className="h-5 w-5" />
+                <span className="sr-only">تلگرام</span>
+              </Link>
+            </div>
           </div>
-          <p className="text-sm text-muted-foreground">
-            پلتفرم جامع برگزاری و مدیریت رویدادهای آموزشی، علمی و تجاری
-          </p>
-          <div className="flex justify-center gap-6">
-            <Link
-              href="https://t.me/hamgerd_ir"
-              className="text-muted-foreground hover:text-foreground"
+
+          <div className="space-y-4">
+            <h3 className="font-bold text-lg">دسترسی سریع</h3>
+            <ul className="space-y-2">
+              <li>
+                <Link
+                  href="/"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  صفحه اصلی
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/events"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  رویدادها
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/organizations"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  سازمان‌ها
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/about"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  درباره ما
+                </Link>
+              </li>
+              <li></li>
+            </ul>
+          </div>
+
+          <div className="space-y-4">
+            <h3 className="font-bold text-lg">خدمات</h3>
+            <ul className="space-y-2">
+              <li>
+                <Link
+                  href="/new-event"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  ایجاد رویداد
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/new-organization"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  ثبت سازمان
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/dashboard"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  داشبورد کاربری
+                </Link>
+              </li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+
+          <div className="space-y-4">
+            <h3 className="font-bold text-lg">تماس با ما</h3>
+            <ul className="space-y-3">
+              <li className="flex items-center gap-2 text-muted-foreground">
+                <MapPin className="h-4 w-4 shrink-0" />
+                <span>ارومیه، میدان ولایت فقیه، خ مطهری، کوچه گمرک</span>
+              </li>
+              <li className="flex items-center gap-2 text-muted-foreground">
+                <Phone className="h-4 w-4 shrink-0" />
+                <a href="tel:09964066461">09964066461</a>
+              </li>
+              <li className="flex items-center gap-2 text-muted-foreground">
+                <Mail className="h-4 w-4 shrink-0" />
+                <span>info@hamgerd.ir</span>
+              </li>
+            </ul>
+          </div>
+          <div className="flex justify-center items-center">
+            <a
+              referrerPolicy="origin"
+              target="_blank"
+              href="https://trustseal.enamad.ir/?id=615587&Code=Qw0Ofji05K3rhWG4GTTm5LZMuLYnr6wB"
             >
-              <FaTelegram className="h-5 w-5" />
-              <span className="sr-only">تلگرام</span>
-            </Link>
+              <img
+                referrerPolicy="origin"
+                src="https://trustseal.enamad.ir/logo.aspx?id=615587&Code=Qw0Ofji05K3rhWG4GTTm5LZMuLYnr6wB"
+                alt=""
+                code="Qw0Ofji05K3rhWG4GTTm5LZMuLYnr6wB"
+                className="h-20 w-20"
+              />
+            </a>
           </div>
-        </div>
-
-        <div className="space-y-4">
-          <h3 className="font-bold text-lg">دسترسی سریع</h3>
-          <ul className="space-y-2">
-            <li>
-              <Link
-                href="/"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                صفحه اصلی
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/events"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                رویدادها
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/organizations"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                سازمان‌ها
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/about"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                درباره ما
-              </Link>
-            </li>
-            <li></li>
-          </ul>
-        </div>
-
-        <div className="space-y-4">
-          <h3 className="font-bold text-lg">خدمات</h3>
-          <ul className="space-y-2">
-            <li>
-              <Link
-                href="/new-event"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                ایجاد رویداد
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/new-organization"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                ثبت سازمان
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/dashboard"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                داشبورد کاربری
-              </Link>
-            </li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-
-        <div className="space-y-4">
-          <h3 className="font-bold text-lg">تماس با ما</h3>
-          <ul className="space-y-3">
-            <li className="flex items-center gap-2 text-muted-foreground">
-              <MapPin className="h-4 w-4 shrink-0" />
-              <span>ارومیه، میدان ولایت فقیه، خ مطهری، کوچه گمرک</span>
-            </li>
-            <li className="flex items-center gap-2 text-muted-foreground">
-              <Phone className="h-4 w-4 shrink-0" />
-              <a href="tel:09964066461">09964066461</a>
-            </li>
-            <li className="flex items-center gap-2 text-muted-foreground">
-              <Mail className="h-4 w-4 shrink-0" />
-              <span>info@hamgerd.ir</span>
-            </li>
-          </ul>
         </div>
       </div>
-
       <div className="container mt-12 pt-8 border-t px-6  mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-center gap-6">
           <p className="text-sm text-muted-foreground">
