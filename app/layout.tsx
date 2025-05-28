e97b70867ddd82a811e65c028dadb6b793ac723c
Jack <jack.70aw@gmail.com>
2025-05-28 17:03:19 +0330

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
-import type React from "react"
-import "@/app/globals.css"
-import { Mona_Sans as FontSans } from "next/font/google"
-import type { Metadata } from "next"
-import { cn } from "@/lib/utils"
-import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/toaster"
-import { siteConfig } from "@/config/site"
-import Header from "@/components/header"
-import Footer from "@/components/footer"
+import type React from "react";
+import "@/app/globals.css";
+import { Mona_Sans as FontSans } from "next/font/google";
+import type { Metadata } from "next";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+import { siteConfig } from "@/config/site";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import localFont from "next/font/local";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: {
@@ -20,18 +21,32 @@ export const metadata: Metadata = {
     template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
-    generator: 'v0.dev'
-}
+};
+
+const vazir = localFont({
+  src: "../fonts/Vazirmatn.ttf",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="fa" dir="rtl" suppressHydrationWarning>
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          vazir.className
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           <div className="relative flex min-h-screen flex-col">
             <Header />
             <div className="flex-1">{children}</div>
@@ -41,5 +56,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
