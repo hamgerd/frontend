48edaa79edfefe7ed6932e98769c7f5019170fbc
Jack <jack.70aw@gmail.com>
2025-07-24 16:58:38 +0330

    fix: auth for all page of dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,8 +2,23 @@
 
 import React from "react";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { useEffect, useState } from "react";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const [authorized, setAuthorized] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      window.location.href = "/";
+    } else {
+      setAuthorized(true);
+    }
+  }, []);
+
+  if (authorized === null) {
+    return null;
+  }
   return (
     <div className="fixed inset-0 z-50 overflow-y-auto bg-background">
       <SidebarProvider>{children}</SidebarProvider>
