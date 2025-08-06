c1f672916013adf9ccf62a5ffdd759f7e7465dc9
Jack <jack.70aw@gmail.com>
2025-08-06 13:36:47 +0330

    feat: add loading page

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+export default function loading() {
+  return (
+    <div className="wrapper m-auto flex min-h-screen w-full items-center justify-center">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="shadow"></div>
+      <div className="shadow"></div>
+      <div className="shadow"></div>
+    </div>
+  );
+}
