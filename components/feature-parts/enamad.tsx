e6c79b63aa26a53c07e9b0dcc9bd12336dc31547
Jack <jack.70aw@gmail.com>
2025-08-14 19:36:21 +0330

    fix: enamad

--- components/feature-parts/enamad.tsx
+++ components/feature-parts/enamad.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Enamad() {
   return (
+    // eslint-disable-next-line @eslint-react/dom/no-unsafe-target-blank
     <div className="flex items-center justify-center">
       <a
         href="https://trustseal.enamad.ir/?id=615587&Code=Qw0Ofji05K3rhWG4GTTm5LZMuLYnr6wB"
-        rel="noreferrer noopener"
         target="_blank"
       >
         <Image
